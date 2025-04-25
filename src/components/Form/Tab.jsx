8c6cdbbcd84642422422a04ba22221f2ff62cd09
AdityaK0 <aditya123905@gmail.com>
2025-04-25 18:11:06 +0530

    css

--- src/components/Form/Tab.jsx
+++ src/components/Form/Tab.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Tab({title,handleTab,isActive}) {
   return (
-    <div className='flex rounded justify-center cursor-pointer shadow-black shadow-xs min-h-10 min-w-52 ' style={{background:isActive?"gray":""}} onClick={handleTab}>
+    <div className='flex rounded border-l-2 justify-center cursor-pointer shadow-black shadow-xs min-h-10 min-w-52 ' style={{background:isActive?"gray":"",boxShadow:isActive?"4px 4px black":""}} onClick={handleTab}>
       <span className='text-center m-auto'>{title}</span>
     </div>
   )
